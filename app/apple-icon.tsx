bbfff15672d2cb3595795a63076d4a857871b1fa
Kenta Mori <morinpic0518@gmail.com>
2026-03-04 05:35:19 +0900

    ファビコンとApple Touch Iconを追加

---
+++ app/apple-icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 180, height: 180 };
+export const contentType = "image/png";
+
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: 180,
+          height: 180,
+          background: "#008080",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 24,
+        }}
+      >
+        <span
+          style={{
+            color: "white",
+            fontSize: 72,
+            fontWeight: 700,
+            letterSpacing: "-2px",
+          }}
+        >
+          t.
+        </span>
+      </div>
+    ),
+    { ...size }
+  );
+}
